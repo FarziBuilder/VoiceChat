b45286b05a5cdc1f768070f49efe7d253fc84bb3
FarziBuilder <112924717+FarziBuilder@users.noreply.github.com>
2024-06-06 15:59:14 +0530

    Update voiceChatClient.py

--- voiceChatClient.py
+++ voiceChatClient.py
@@ -4,7 +4,7 @@ import pyaudio
 
 client = socket.socket()
 
-host = "35.164.39.119"
+host = "3.111.198.149"
 port = 5000
 
 client.connect((host, port))
@@ -13,23 +13,23 @@ p = pyaudio.PyAudio()
 
 Format = pyaudio.paInt16
 Chunks = 4096
-Channels = 2
+Channels = 1  # Change from 2 to 1
 Rate = 44100
 
-input_stream = p.open(format = Format,
-                      channels = Channels,
-                      rate = Rate,
-                      input = True,
-                      frames_per_buffer = Chunks)
+input_stream = p.open(format=Format,
+                      channels=Channels,
+                      rate=Rate,
+                      input=True,
+                      frames_per_buffer=Chunks)
 
-output_stream = p.open(format = Format,
-                      channels = Channels,
-                      rate = Rate,
-                      output = True,
-                      frames_per_buffer = Chunks)
+output_stream = p.open(format=Format,
+                       channels=Channels,
+                       rate=Rate,
+                       output=True,
+                       frames_per_buffer=Chunks)
 
 def send():
-    while(True):
+    while True:
         try:
             data = input_stream.read(Chunks)
             client.send(data)
@@ -37,15 +37,15 @@ def send():
             break
 
 def receive():
-    while(True):
+    while True:
         try:
             data = client.recv(Chunks)
             output_stream.write(data)
         except:
             break
 
-t1 = threading.Thread(target = send)
-t2 = threading.Thread(target = receive)
+t1 = threading.Thread(target=send)
+t2 = threading.Thread(target=receive)
 
 t1.start()
 t2.start()
@@ -58,31 +58,3 @@ input_stream.close()
 output_stream.stop()
 output_stream.close()
 p.terminate()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
