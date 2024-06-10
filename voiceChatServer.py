dc15ba7b90ff8ef564011e9f0285e8cbc5a4d2f4
FarziBuilder <112924717+FarziBuilder@users.noreply.github.com>
2024-06-10 20:17:51 +0530

    Update voiceChatServer.py

--- voiceChatServer.py
+++ voiceChatServer.py
@@ -8,25 +8,32 @@ server = socket.socket()
 server.bind((host, port))
 server.listen(5)
 
-clients = []
+clients = {}
 
 def start():
     print("Server started, waiting for connections...")
     while True:
         conn, addr = server.accept()
         print(f"Client connected from {addr}")
-        clients.append(conn)
-        t = threading.Thread(target=handle_client, args=(conn,))
+        
+        check_value = conn.recv(4096).decode()
+        own_value, pair_value = check_value.split(',')
+        clients[own_value] = conn
+        
+        #clients.append(conn)
+        t = threading.Thread(target=handle_client, args=(conn,pair_value))
         t.start()
 
-def handle_client(conn):
+def handle_client(conn,pair_value):
     try:
-        while True:
+        #check = 0
+        while True:              
             data = conn.recv(4096)
             if not data:
                 break
+            
             print(f"Received data from {conn.getpeername()}")
-            broadcast(data, conn)
+            broadcast(data, conn, pair_value)
     except Exception as e:
         print(f"Client handling error: {e}")
     finally:
@@ -34,13 +41,14 @@ def handle_client(conn):
         clients.remove(conn)
         conn.close()
 
-def broadcast(data, from_conn):
-    for cl in clients:
-        if cl != from_conn:
-            try:
-                cl.send(data)
-                print("Broadcasted data")
-            except Exception as e:
-                print(f"Broadcast error: {e}")
-
+def broadcast(data, from_conn, pair_value):
+    
+    cl = clients[pair_value]
+    try:
+        cl.send(data)
+        print("Broadcasted data")
+    except Exception as e:
+        print("Broadcast error: {e}")
+        
 start()
+
